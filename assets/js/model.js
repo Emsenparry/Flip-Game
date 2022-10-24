6b0dc3aab6b4dc80dd48ff62ecd3396776823f90
Emma Nielsen <Emmanielsen843@gmail.com>
2022-10-24 10:24:06 +0200

    Code along
    Ended up doing some code along with Heinz

---
+++ assets/js/model.js
@@ -0,0 +1,92 @@
+export const GoalModel = () => {
+    return (
+    [  
+        {
+            title: "Afskaf Fattigdom",
+            goal: 1,
+            picture: '/assets/images/1.jpg'
+        },
+        {
+            title: "Stop Sult",
+            goal: 2,
+            picture: '/assets/images/2.jpg'
+        },
+        {
+            title: "Sundhed Og Trivsel",
+            goal: 3,
+            picture: '/assets/images/3.jpg'
+        },
+        {
+            title: "Kvalitets-Uddannelse",
+            goal: 4,
+            picture: '/assets/images/4.jpg'
+        },
+        {
+            title: "Ligestilling Mellem Kønnene",
+            goal: 5,
+            picture: '/assets/images/5.jpg'
+        },
+        {
+            title: "Rent Vand Og Sanitet",
+            goal: 6,
+            picture: '/assets/images/6.jpg'
+        },
+        {
+            title: "Bæredygtig Energi",
+            goal: 7,
+            picture: '/assets/images/7.jpg'
+        },
+        {
+            title: "Anstændige Jobs og Økonomisk Vækst",
+            goal: 8,
+            picture: '/assets/images/8.jpg'
+        },
+        {
+            title: "Industri, Innovation Og Infrastruktur",
+            goal: 9,
+            picture: '/assets/images/9.jpg'
+        },
+        {
+            title: "Mindre Ulighed",
+            goal: 10,
+            picture: '/assets/images/10.jpg'
+        },
+        {
+            title: "Bæredygtige Byer Og Lokalsamfund",
+            goal: 11,
+            picture: '/assets/images/11.jpg'
+        },
+        {
+            title: "Ansvarligt Forbrug Og Produktion",
+            goal: 12,
+            picture: '/assets/images/12.jpg'
+        },
+        {
+            title: "Klima-Indsats",
+            goal: 13,
+            picture: '/assets/images/13.jpg'
+        },
+        {
+            title: "Livet I Havet",
+            goal: 14,
+            picture: '/assets/images/14.jpg'
+        },
+        {
+            title: "Livet På Land",
+            goal: 15,
+            picture: '/assets/images/15.jpg'
+        },
+        {
+            title: "Fred, Retfærdighed Og Stærke Instituioner",
+            goal: 16,
+            picture: '/assets/images/16.jpg'
+        },
+        {
+            title: "Partnerskaber For Handling",
+            goal: 17,
+            picture: '/assets/images/17.jpg'
+        }  
+    ]
+)
+}
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,89 +1,20 @@
-let arr_goals = [
-    {
-        title: "Afskaf Fattigdom",
-        number: 1,
-        picture: '/assets/images/1.jpg'
-    },
-    {
-        title: "Stop Sult",
-        number: 2,
-        picture: '/assets/images/2.jpg'
-    },
-    {
-        title: "Sundhed Og Trivsel",
-        number: 3,
-        picture: '/assets/images/3.jpg'
-    },
-    {
-        title: "Kvalitets-Uddannelse",
-        number: 4,
-        picture: '/assets/images/4.jpg'
-    },
-    {
-        title: "Ligestilling Mellem Kønnene",
-        number: 5,
-        picture: '/assets/images/5.jpg'
-    },
-    {
-        title: "Rent Vand Og Sanitet",
-        number: 6,
-        picture: '/assets/images/6.jpg'
-    },
-    {
-        title: "Bæredygtig Energi",
-        number: 7,
-        picture: '/assets/images/7.jpg'
-    },
-    {
-        title: "Anstændige Jobs og Økonomisk Vækst",
-        number: 8,
-        picture: '/assets/images/8.jpg'
-    },
-    {
-        title: "Industri, Innovation Og Infrastruktur",
-        number: 9,
-        picture: '/assets/images/9.jpg'
-    },
-    {
-        title: "Mindre Ulighed",
-        number: 10,
-        picture: '/assets/images/10.jpg'
-    },
-    {
-        title: "Bæredygtige Byer Og Lokalsamfund",
-        number: 11,
-        picture: '/assets/images/11.jpg'
-    },
-    {
-        title: "Ansvarligt Forbrug Og Produktion",
-        number: 12,
-        picture: '/assets/images/12.jpg'
-    },
-    {
-        title: "Klima-Indsats",
-        number: 13,
-        picture: '/assets/images/13.jpg'
-    },
-    {
-        title: "Livet I Havet",
-        number: 14,
-        picture: '/assets/images/14.jpg'
-    },
-    {
-        title: "Livet På Land",
-        number: 15,
-        picture: '/assets/images/15.jpg'
-    },
-    {
-        title: "Fred, Retfærdighed Og Stærke Instituioner",
-        number: 16,
-        picture: '/assets/images/16.jpg'
-    },
-    {
-        title: "Partnerskaber For Handling",
-        number: 17,
-        picture: '/assets/images/17.jpg'
-    }
-    
-]
-console.log(arr_goals);
+import { GoalModel } from "./model.js"
+
+const gameboard = document.getElementById('gameboard');
+const numCards = 5;
+let cardList = GoalModel()
+//Randomizer arrayets goals
+cardList.sort(() => Math.random() - 0.5);
+cardList = cardList.slice(0, numCards)
+//Sammenkæder arrayet med sig selv
+cardList.concat(cardList);
+cardList.sort(() => Math.random() - 0.5);
+
+for(let card of cardList) { 
+    let div = document.createElement('div'); //Skaber en div
+    div.innerText = card.goal
+    div.innerHTML = card.picture
+    gameboard.append(div) //Prepend - Før div. Append - Efter div
+}
+
+console.log(GoalModel().slice(15));
