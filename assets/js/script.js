88e601ca9528c5855ea88e2093392aff587a2075
Emma Nielsen <Emmanielsen843@gmail.com>
2022-10-24 13:24:31 +0200

    css
    css changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
 import { GoalModel } from "./model.js"
 
 const gameboard = document.getElementById('gameboard');
-const numCards = 20; //Vælg hvor mange 
+const numCards = 10; //Vælg hvor mange brikker der er
 let cardList = GoalModel() //Vores array fra model.js
 //Randomizer arrayets goals
-cardList.sort(() => Math.random() - 0.5);
+cardList.sort(() => Math.random() - 0.17);
 cardList = cardList.slice(0, numCards)
 //Sammenkæder arrayet med sig selv
-cardList.concat(cardList);
-cardList.sort(() => Math.random() - 0.5);
+cardList = cardList.concat(cardList);
+cardList.sort(() => Math.random() - 0.12);
 
 for(let card of cardList) { 
-    let div = document.createElement('div'); //Skaber en div
-    div.innerText = card.goal
-    gameboard.append(div); //Prepend - Før div. Append - Efter div
+    // let div = document.createElement('div');
+    // div.innerText = card.goal
+    // gameboard.append(div);
     
-    let img = document.createElement('img');
+    let img = document.createElement('img'); //Skaber en div
     img.src = card.picture;
-    gameboard.append(img);
-}
+    gameboard.append(img); //Prepend - Før div. Append - Efter div
 
+}
 console.log(GoalModel());
